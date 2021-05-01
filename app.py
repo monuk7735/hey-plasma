c7e39fc8e5b17c58720f95fc8e88e1268211db69
Monu Kumar <monuk7735@gmail.com>
2021-05-01 12:59:48 +0530

    Statics

--- app.py
+++ app.py
@@ -2,22 +2,29 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return render_template('home.html')
 
+
 @app.route("/register")
 def register():
     return "register"
-    
+
+
 @app.route("/login")
 def login():
     return "login"
 
+
 @app.route("/requests")
 def connected_users():
     return "connected users"
-    
-    
+
+
 if __name__ == "__main__":
-    app.run("0.0.0.0",7735)
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+    app.run("0.0.0.0", 7735)
