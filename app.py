967ef3636c069325d55ac712578158427c1c84a1
Monu Kumar <monuk7735@gmail.com>
2021-05-02 00:51:18 +0530

    Colors + Home + Login

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template, redirect
+from pprint import pprint
 
 app = Flask(__name__)
 
@@ -19,14 +20,27 @@ def home():
 @app.route("/register", methods=['POST', 'GET'])
 def register():
     if request.method == "POST":
+        name = request.form['name']
+        blood_group = request.form['blood']
+        address_city = request.form['address-city']
+        address_state = request.form['address-state']
+        address_pin = request.form['address-pin']
+        email = request.form['email']
+        password = request.form['password']
+        # TODO Do registration
         return "doing registration"
 
-    return "register"
+    return redirect("/login")
 
 
-@app.route("/login")
+@app.route("/login", methods=['POST', 'GET'])
 def login():
-    return "login"
+    if request.method == "POST":
+        email = request.form['email']
+        password = request.form['password']
+        # TODO Do Login
+        return "doing login"
+    return render_template("login.html")
 
 
 @app.route("/requests")
